834bc209b925c2a26b5018548c3c0e2144e6375d
adamleithp <adamleithp@gmail.com>
2024-08-24 20:00:36 +0100

    forward ref for input password example

--- components/examples/input-password.tsx
+++ components/examples/input-password.tsx
@@ -4,39 +4,51 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-export function InputPassword() {
-  const [showPassword, setShowPassword] = useState(false);
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-  return (
-    <label htmlFor="password" className="block dark space-y-1">
-      <Label htmlFor="password">Password</Label>
-      <div className="relative">
-        <Input
-          type={showPassword ? "text" : "password"}
-          defaultValue={"some-super-password"}
-          id="password"
-          className="pr-10"
-          name="username"
-        />
-        <Button
-          className="absolute inset-0 left-auto  h-full"
-          onClick={() => {
-            setShowPassword(!showPassword);
-          }}
-          aria-label="Toggle password visibility"
-          aria-pressed={showPassword}
-          aria-controls="password"
-          size="icon"
-          variant={"ghost"}
-        >
-          {showPassword ? (
-            <EyeOff color="white" strokeWidth="1" size={16} />
-          ) : (
-            <EyeIcon color="white" strokeWidth="1" size={16} />
-          )}
-        </Button>
-      </div>
-    </label>
-  );
-}
+const InputPassword = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, id, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+      <label htmlFor={id} className="block dark space-y-1">
+        <Label htmlFor={id}>Password</Label>
+        <div className="relative">
+          <Input
+            type={showPassword ? "text" : "password"}
+            defaultValue={"some-super-password"}
+            id={id}
+            className={cn("pr-10", className)}
+            ref={ref}
+            {...props}
+          />
+          <Button
+            className="absolute inset-0 left-auto  h-full"
+            onClick={() => {
+              setShowPassword(!showPassword);
+            }}
+            aria-label="Toggle password visibility"
+            aria-pressed={showPassword}
+            aria-controls={id}
+            size="icon"
+            variant={"ghost"}
+          >
+            {showPassword ? (
+              <EyeOff color="white" strokeWidth="1" size={16} />
+            ) : (
+              <EyeIcon color="white" strokeWidth="1" size={16} />
+            )}
+          </Button>
+        </div>
+      </label>
+    );
+  }
+);
+
+InputPassword.displayName = "InputPassword";
+
+export { InputPassword };
