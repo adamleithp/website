71ab56fff8789249df61bd5adc17a34bb2cffb73
adamleithp <adamleithp@gmail.com>
2024-08-29 13:53:39 +0100

    trying edge

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -2,7 +2,7 @@ import { OpenGraphLayout } from "@/components/sections/opengraph-layout";
 import { OpenGraphSize, boldFont, regularFont } from "@/lib/opengraph";
 import { ImageResponse } from "next/og";
 
-export const runtime = "nodejs";
+export const runtime = "edge";
 
 export default async function OpenGraphImage() {
   const [regularFontData, boldFontData] = await Promise.all([
