3b1862087025237aabfcc8fabbe6e19a06a97f62
adamleithp <adamleithp@gmail.com>
2024-09-08 22:56:01 +0100

    added extending sonner blog post

--- components/sections/markdown-renderer.tsx
+++ components/sections/markdown-renderer.tsx
@@ -1,17 +1,17 @@
+import FileCodeBlock from "@/components/ui/file-code-block";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import rehypePrism from "rehype-prism-plus";
 import remarkGfm from "remark-gfm";
-import { mdxComponents } from "./markdown-components";
-import FileCodeBlock from "@/components/ui/file-code-block";
-import { ElementShowcase } from "./element-showcase";
 import { InputPassword } from "../examples/input-password";
-import { TextSkeleton } from "../examples/text-skeleton";
-import { TextSkeletonExample } from "../examples/text-skeleton-example";
 import { PricingForm as PricingFormJustDom } from "../examples/pricing-form/pricing-form-just-dom";
 import { PricingForm as PricingFormJustDomWithCard } from "../examples/pricing-form/pricing-form-just-dom-and-card";
 import { PricingForm as PricingFormJustDomWithCardWithRHF } from "../examples/pricing-form/pricing-form-just-dom-and-card-rhf";
-import { RadioGroupItem } from "../examples/pricing-form/radio-group-item";
 import { RadioGroup } from "../examples/pricing-form/radio-group";
+import { RadioGroupItem } from "../examples/pricing-form/radio-group-item";
+import { TextSkeleton } from "../examples/text-skeleton";
+import { TextSkeletonExample } from "../examples/text-skeleton-example";
+import { ElementShowcase } from "./element-showcase";
+import { mdxComponents } from "./markdown-components";
 
 export function MarkdownRenderer({ content }: { content: string }) {
   return (
